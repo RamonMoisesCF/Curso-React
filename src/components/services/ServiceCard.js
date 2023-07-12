5d524c14ac024220e65d68b096fd4f075eb502b5
RamonMCF <moises.colim@gmail.com>
2023-07-12 11:20:47 -0300

    Remoção de bugs

--- src/components/services/ServiceCard.js
+++ src/components/services/ServiceCard.js
@@ -2,13 +2,17 @@ import styles from "../project/ProjectCard.module.css";
 import { BsFillTrashFill } from "react-icons/bs";
 
 function ServiceCard({ id, name, cost, description, handleRemove }) {
-  const remove = (e) => {};
+  const remove = (e) => {
+    e.preventDefault()
+    handleRemove(id, cost)
+
+  };
 
   return (
     <div className={styles.project_card}>
       <h4>{name}</h4>
       <p>
-        <span>Costo total:</span> R${cost}
+        <span>Custo total:</span> R${cost}
       </p>
       <p>{description}</p>
       <div className={styles.project_card_actions}>
