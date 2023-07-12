048c94e785f10bf09436677352fe71f14ee05266
RamonMCF <moises.colim@gmail.com>
2023-07-12 10:08:11 -0300

    Subindo repo

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
+
+import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+
+function App() {
+  return (
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/company">
+            <Company />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/newproject">
+            <NewProject />
+          </Route>
+          <Route path="/project/:id">
+            <Project />
+          </Route>
+        </Container>
+      </Switch>
+      <Footer/>
+    </Router>
+  )
+}
+
+export default App;
